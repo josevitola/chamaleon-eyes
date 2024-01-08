6a813f07c6438fa349d811194302f2362acbdd26
Jose Vitola <jdnietov@unal.edu.co>
2024-01-07 21:12:41 -0500

    Add Canvas component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Canvas } from './components/Canvas';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = '#ffffff';
+    ctx.beginPath();
+    ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+    ctx.fill();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Canvas animated width={600} height={300} draw={draw} />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Canvas/Canvas.tsx
@@ -0,0 +1,34 @@
+import React, { useRef, useEffect } from 'react';
+
+interface CanvasProps extends React.CanvasHTMLAttributes<HTMLCanvasElement> {
+  draw: (ctx: CanvasRenderingContext2D, frameCount: number) => void;
+  animated?: boolean;
+}
+
+const Canvas = ({ draw, animated, ...rest }: CanvasProps) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext('2d') as CanvasRenderingContext2D;
+
+    let frameCount = 0;
+    let animationFrameId: number;
+
+    const render = () => {
+      if (animated) frameCount++;
+      draw(context, frameCount);
+      animationFrameId = window.requestAnimationFrame(render);
+    };
+
+    render();
+
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    };
+  }, [animated]);
+
+  return <canvas ref={canvasRef} {...rest} />;
+};
+
+export default Canvas;
