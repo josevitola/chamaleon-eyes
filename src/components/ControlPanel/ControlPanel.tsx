77ca25209b6f8c8531e34d4937cc1f1a0fb53c3f
José Vitola <jdnv.123@gmail.com>
2026-01-01 02:52:55 -0500

    Add .prettierrc

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -1,16 +1,31 @@
-import { useContext } from "react";
-import { AppContext } from "../../App.context";
-import { Box } from "../Box";
-import { ToggleButton } from "../ToggleButton/ToggleButton";
+import { useContext } from 'react';
+import { AppContext } from '../../App.context';
+import { Box } from '../Box';
+import { ToggleButton } from '../ToggleButton/ToggleButton';
 
 export const ControlPanel = () => {
-  const { isAnimationEnabled, setIsAnimationEnabled, isDebugEnabled, setDebugEnabled } = useContext(AppContext);
+  const {
+    isAnimationEnabled,
+    setIsAnimationEnabled,
+    isDebugEnabled,
+    setDebugEnabled,
+  } = useContext(AppContext);
 
   return (
     <Box style={{ height: '500px', width: '300px' }}>
-      <ToggleButton onLabel="playing" offLabel="paused" onClick={() => setIsAnimationEnabled(!isAnimationEnabled)} checked={isAnimationEnabled} />
+      <ToggleButton
+        onLabel="playing"
+        offLabel="paused"
+        onClick={() => setIsAnimationEnabled(!isAnimationEnabled)}
+        checked={isAnimationEnabled}
+      />
       &nbsp;
-      <ToggleButton onLabel="debug" offLabel="debug" onClick={() => setDebugEnabled(!isDebugEnabled)} checked={isDebugEnabled} />
+      <ToggleButton
+        onLabel="debug"
+        offLabel="debug"
+        onClick={() => setDebugEnabled(!isDebugEnabled)}
+        checked={isDebugEnabled}
+      />
     </Box>
   );
 };
